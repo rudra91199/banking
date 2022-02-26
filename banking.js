30ac121c25851c1a613aa5bfd9d929a5f2163e54
rudra91199 <rskrrudra9111999@gmail.com>
2022-02-26 19:11:48 +0600

    responsive

--- banking.js
+++ banking.js
@@ -1,49 +1,81 @@
 const depositAmount = document.getElementById('deposit-amount');
 const withdrawAmount = document.getElementById('withdraw-amount');
 const balance = document.getElementById('balance');
-const totalWithdraw= document.getElementById('total-withdraw');
-const totalDeposit= document.getElementById('total-deposit');
+const totalWithdraw = document.getElementById('total-withdraw');
+const totalDeposit = document.getElementById('total-deposit');
 depositAmount.defaultValue = 0
+withdrawAmount.defaultValue = 0
 
 document.getElementById('deposit').addEventListener('click', function () {
     newDepositValue = parseFloat(depositAmount.value);
-    balanceValue = parseFloat(balance.innerText);
-    prevDepositValue = parseFloat(totalDeposit.innerText);
 
-    totalDepositValue = prevDepositValue + newDepositValue;
-   
-    totalDeposit.innerText = totalDepositValue;
+    if (newDepositValue >= 0) {
+        balanceValue = parseFloat(balance.innerText);
+        prevDepositValue = parseFloat(totalDeposit.innerText);
+        totalDepositValue = prevDepositValue + newDepositValue;
+        totalDeposit.innerText = totalDepositValue;
+        newBalanceValue = balanceValue + newDepositValue;
+        balance.innerText = newBalanceValue;
+        depositAmount.value = "0";
+    }
+    else {
+        withdrawError = document.createElement('p');
+        withdrawError.innerText = "Please enter an integer value"
+        withdrawError.style.color = '#ffd60a';
+        withdrawError.style.fontWeight = 'bold';
+        withdrawError.style.fontSize = '0.5em';
+        withdrawError.style.marginTop = '5px';
+        document.getElementById('deposit-container').appendChild(withdrawError);
+    }
 
-    newBalanceValue = balanceValue + newDepositValue;
-    balance.innerText = newBalanceValue;
     depositAmount.value = "0";
+
 })
+
+
+document.getElementById('deposit').addEventListener('blur', function () {
+    withdrawError.style.display = 'none';
+})
+
 
 document.getElementById('withdraw').addEventListener('click', function () {
     newWithdrawValue = parseFloat(withdrawAmount.value);
     prevWithdrawValue = parseFloat(totalWithdraw.innerText);
     balanceValue = parseFloat(balance.innerText);
 
-    if ((balanceValue - newWithdrawValue) >= 0) {       
-        totalWithdrawValue = prevWithdrawValue + newWithdrawValue;
-        totalWithdraw.innerText = totalWithdrawValue;
-        newBalanceValue = balanceValue - newWithdrawValue;
-        balance.innerText = newBalanceValue;
+    if (newWithdrawValue >= 0) {
+        if ((balanceValue - newWithdrawValue) >= 0) {
+            totalWithdrawValue = prevWithdrawValue + newWithdrawValue;
+            totalWithdraw.innerText = totalWithdrawValue;
+            newBalanceValue = balanceValue - newWithdrawValue;
+            balance.innerText = newBalanceValue;
+        }
+
+        else {
+            withdrawError = document.createElement('p');
+            withdrawError.innerText = "You don't have Sufficient Balance"
+            withdrawError.style.color = '#ffc300';
+            withdrawError.style.fontSize = '0.5em';
+            withdrawError.style.marginTop = '5px';
+            document.getElementById('withdraw-container').appendChild(withdrawError);
+        }
     }
 
     else {
         withdrawError = document.createElement('p');
-        withdrawError.innerText = "You don't have Sufficient Balance"
-        withdrawError.style.color = '#ffc300';
+        withdrawError.innerText = "Please enter an integer value"
+        withdrawError.style.color = '#ffd60a';
+        withdrawError.style.fontWeight = 'bold';
         withdrawError.style.fontSize = '0.5em';
         withdrawError.style.marginTop = '5px';
-        document.getElementById('withdraw-container').appendChild(withdrawError); 
+        document.getElementById('withdraw-container').appendChild(withdrawError);
     }
 
+    withdrawAmount.value = "0";
 })
 
 document.getElementById('withdraw').addEventListener('blur', function () {
     withdrawError.style.display = 'none';
- })
+})
 
 
